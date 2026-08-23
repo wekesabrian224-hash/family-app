6f3952305170bf6954b5adcf43eb183c4502ad9b
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-08-23 08:26:33 +0300

    added the family members data structure

---
+++ src/data/family.js
@@ -0,0 +1,43 @@
+export const familyMembers = [
+  {
+    id: "dad",
+    name: "Dad",
+    role: "Father",
+    image: "/images/dad.jpg",
+  },
+
+  {
+    id: "mum",
+    name: "Mum",
+    role: "Mother",
+    image: "/images/mum.jpg",
+  },
+
+  {
+    id: "lucky",
+    name: "Lucky",
+    role: "Family Member",
+    image: "/images/lucky.jpg",
+  },
+
+  {
+    id: "jones",
+    name: "Jones",
+    role: "Family Member",
+    image: "/images/jones.jpg",
+  },
+
+  {
+    id: "esther",
+    name: "Esther",
+    role: "Family Member",
+    image: "/images/esther.jpg",
+  },
+
+  {
+    id: "sifa",
+    name: "Sifa",
+    role: "Family Member",
+    image: "/images/sifa.jpg",
+  },
+];
